6a6996d521baa33e7b0e5047c23e2b0781fe45e0
MPD97 <mateuszpracadom@gmail.com>
2020-06-14 15:39:13 +0200

    Added Banner DbSet in AdvertContext

--- APBD-Advert-Project/AdvertDatabaseCL/Contexts/AdvertContext.cs
+++ APBD-Advert-Project/AdvertDatabaseCL/Contexts/AdvertContext.cs
@@ -13,6 +13,8 @@ namespace AdvertDatabaseCL.Contexts
         public DbSet<Client> Clients { get; set; }
         public DbSet<Building> Buildings { get; set; }
         public DbSet<Campaign> Campaigns { get; set; }
+        public DbSet<Banner> Banners { get; set; }
+
 
         public AdvertContext([NotNullAttribute] DbContextOptions options) : base(options)
         {
