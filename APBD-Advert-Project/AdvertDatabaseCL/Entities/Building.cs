55dbc1607041f8c6f1b968859f2552c60aa52400
MPD97 <mateuszpracadom@gmail.com>
2020-06-14 14:31:53 +0200

    Added Building class with basic properties

---
+++ APBD-Advert-Project/AdvertDatabaseCL/Entities/Building.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdvertDatabaseCL.Entities
+{
+    public class Building
+    {
+        public int IdBuilding { get; set; }
+        public string Street { get; set; }
+        public int StreetNumber { get; set; }
+        public string City { get; set; }
+        public decimal Height { get; set; }
+    }
+}
