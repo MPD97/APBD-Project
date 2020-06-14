248000a3e8629215b4ad41a8cdfafe1a25e9c261
MPD97 <mateuszpracadom@gmail.com>
2020-06-14 14:45:49 +0200

    Added AddedBasicBuildingTable migration

---
+++ APBD-Advert-Project/AdvertDatabaseCL/Migrations/20200614124500_AddedBasicBuildingTable.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AdvertDatabaseCL.Migrations
+{
+    public partial class AddedBasicBuildingTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Buildings",
+                columns: table => new
+                {
+                    IdBuilding = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Street = table.Column<string>(nullable: true),
+                    StreetNumber = table.Column<int>(nullable: false),
+                    City = table.Column<string>(nullable: true),
+                    Height = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Buildings", x => x.IdBuilding);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Buildings");
+        }
+    }
+}
