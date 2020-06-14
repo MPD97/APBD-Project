959a38f5605b36b3842f595c8810d093a8cd9dae
MPD97 <mateuszpracadom@gmail.com>
2020-06-14 14:15:44 +0200

    Added ClientsEFConfiguration class

---
+++ APBD-Advert-Project/AdvertDatabaseCL/Configurations/ClientsEFConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using AdvertDatabaseCL.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdvertDatabaseCL.Configurations
+{
+    class ClientsEFConfiguration : IEntityTypeConfiguration<Client>
+    {
+        public void Configure(EntityTypeBuilder<Client> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
