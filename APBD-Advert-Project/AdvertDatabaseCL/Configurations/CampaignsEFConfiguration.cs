e53d83318d3caca8d9abefba297b128c3d7204e4
MPD97 <mateuszpracadom@gmail.com>
2020-06-14 15:08:35 +0200

    Added CampaignsEFConfiguration class

---
+++ APBD-Advert-Project/AdvertDatabaseCL/Configurations/CampaignsEFConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using AdvertDatabaseCL.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdvertDatabaseCL.Configurations
+{
+    public class CampaignsEFConfiguration : IEntityTypeConfiguration<Campaign>
+    {
+        public void Configure(EntityTypeBuilder<Campaign> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
