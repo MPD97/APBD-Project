432f38fc705752902e325f169a0987140fdf2d83
MPD97 <mateuszpracadom@gmail.com>
2020-06-14 18:41:24 +0200

    Create IPasswordHasher interface

---
+++ APBD-Advert-Project/Advert.Presistance/Services/IPasswordHasher.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Advert.Presistance.Services
+{
+    public interface IPasswordHasher
+    {
+        public string Create(string value, string salt);
+        public bool Validate(string value, string salt, string hash);
+    }
+}
