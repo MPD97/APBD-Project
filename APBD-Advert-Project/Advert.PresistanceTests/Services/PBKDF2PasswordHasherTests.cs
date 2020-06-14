16463b9ef0a6f516a2335425e40a0d0a25e6ee7c
MPD97 <mateuszpracadom@gmail.com>
2020-06-14 19:25:15 +0200

    Added salt tests

--- APBD-Advert-Project/Advert.PresistanceTests/Services/PBKDF2PasswordHasherTests.cs
+++ APBD-Advert-Project/Advert.PresistanceTests/Services/PBKDF2PasswordHasherTests.cs
@@ -9,6 +9,13 @@ namespace Advert.Presistance.Services.Tests
     [TestClass()]
     public class PBKDF2PasswordHasherTests
     {
+        private readonly IPasswordHasher _passwordHasher;
+
+        public PBKDF2PasswordHasherTests()
+        {
+            _passwordHasher = new PBKDF2PasswordHasher();
+        }
+
         [TestMethod()]
         public void CreateTest()
         {
@@ -19,6 +26,23 @@ namespace Advert.Presistance.Services.Tests
         public void ValidateTest()
         {
             Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void Should_Create_Diffrent_Salt()
+        {
+            string saltOne = _passwordHasher.CreateSalt();
+            string saltTwo = _passwordHasher.CreateSalt();
+
+            Assert.AreNotEqual(saltOne, saltTwo);
+        }
+        [TestMethod()]
+        public void Should_Create_Same_Length_Salt()
+        {
+            string saltOne = _passwordHasher.CreateSalt();
+            string saltTwo = _passwordHasher.CreateSalt();
+            
+            Assert.AreEqual(saltOne.Length, saltTwo.Length);
         }
     }
 }
