4439edaafa6cff6ef0594168913e86a15d9b39d6
MPD97 <mateuszpracadom@gmail.com>
2020-06-14 14:58:48 +0200

    Defined properties of Builiding table by Fluent API in BuildingsEFConfiguration class

--- APBD-Advert-Project/AdvertDatabaseCL/Configurations/BuildingsEFConfiguration.cs
+++ APBD-Advert-Project/AdvertDatabaseCL/Configurations/BuildingsEFConfiguration.cs
@@ -12,6 +12,21 @@ namespace AdvertDatabaseCL.Configurations
         public void Configure(EntityTypeBuilder<Building> builder)
         {
             builder.HasKey(e => e.IdBuilding);
+
+            builder.Property(e => e.Street)
+                .HasMaxLength(100)
+                .IsRequired(true);
+
+            builder.Property(e => e.StreetNumber)
+               .IsRequired(true);
+
+            builder.Property(e => e.City)
+                .HasMaxLength(100)
+                .IsRequired(true);
+
+            builder.Property(e => e.Height)
+                .HasColumnType("decimal(6,2)")
+                .IsRequired(true);
         }
     }
 }
