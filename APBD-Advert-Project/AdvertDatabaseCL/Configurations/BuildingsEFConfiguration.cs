8fb7456a98bf12b8b0b41c4f8d0c7426b58f4a87
MPD97 <mateuszpracadom@gmail.com>
2020-06-14 14:41:26 +0200

    Added BuildingsEFConfiguration class

---
+++ APBD-Advert-Project/AdvertDatabaseCL/Configurations/BuildingsEFConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using AdvertDatabaseCL.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdvertDatabaseCL.Configurations
+{
+    public class BuildingsEFConfiguration : IEntityTypeConfiguration<Building>
+    {
+        public void Configure(EntityTypeBuilder<Building> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
