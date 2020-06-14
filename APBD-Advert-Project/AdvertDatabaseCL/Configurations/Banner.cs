3625fc89dbc0f8ba7bb7c7c29794480912cbc2a9
MPD97 <mateuszpracadom@gmail.com>
2020-06-14 15:35:30 +0200

    Added basic Banner class

---
+++ APBD-Advert-Project/AdvertDatabaseCL/Configurations/Banner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdvertDatabaseCL.Configurations
+{
+    public class Banner
+    {
+        public int IdAdvertisment { get; set; }
+        public int Name { get; set; }
+        public decimal Price { get; set; }
+        public int IdCampaign { get; set; }
+        public decimal Area { get; set; }
+    }
+}
