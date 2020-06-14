84743648d7f1272dfe476b3689d269fbe5f6888a
MPD97 <mateuszpracadom@gmail.com>
2020-06-14 15:06:14 +0200

    Added basic Campaign class

---
+++ APBD-Advert-Project/AdvertDatabaseCL/Entities/Campaign.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdvertDatabaseCL.Entities
+{
+    public class Campaign
+    {
+        public int IdCampaign { get; set; }
+        public int IdClient { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public decimal PricePerSquareMeter { get; set; }
+        public int FromIdBuilding { get; set; }
+        public int ToIdBuilding { get; set; }
+    }
+}
