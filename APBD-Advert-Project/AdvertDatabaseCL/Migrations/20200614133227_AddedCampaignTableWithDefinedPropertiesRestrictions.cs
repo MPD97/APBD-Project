6890991418555d5a19a0a7099256b30e85360d38
MPD97 <mateuszpracadom@gmail.com>
2020-06-14 15:32:55 +0200

    Added AddedCampaignTableWithDefinedPropertiesRestrictions migration

---
+++ APBD-Advert-Project/AdvertDatabaseCL/Migrations/20200614133227_AddedCampaignTableWithDefinedPropertiesRestrictions.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AdvertDatabaseCL.Migrations
+{
+    public partial class AddedCampaignTableWithDefinedPropertiesRestrictions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Campaigns",
+                columns: table => new
+                {
+                    IdCampaign = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdClient = table.Column<int>(nullable: false),
+                    StartDate = table.Column<DateTime>(nullable: false),
+                    EndDate = table.Column<DateTime>(nullable: false),
+                    PricePerSquareMeter = table.Column<decimal>(type: "decimal(6,2)", nullable: false),
+                    FromIdBuilding = table.Column<int>(nullable: false),
+                    ToIdBuilding = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Campaigns", x => x.IdCampaign);
+                    table.ForeignKey(
+                        name: "FK_Campaigns_Buildings_FromIdBuilding",
+                        column: x => x.FromIdBuilding,
+                        principalTable: "Buildings",
+                        principalColumn: "IdBuilding");
+                    table.ForeignKey(
+                        name: "FK_Campaigns_Clients_IdClient",
+                        column: x => x.IdClient,
+                        principalTable: "Clients",
+                        principalColumn: "IdClient");
+                    table.ForeignKey(
+                        name: "FK_Campaigns_Buildings_ToIdBuilding",
+                        column: x => x.ToIdBuilding,
+                        principalTable: "Buildings",
+                        principalColumn: "IdBuilding");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Campaigns_FromIdBuilding",
+                table: "Campaigns",
+                column: "FromIdBuilding");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Campaigns_IdClient",
+                table: "Campaigns",
+                column: "IdClient");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Campaigns_ToIdBuilding",
+                table: "Campaigns",
+                column: "ToIdBuilding");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Campaigns");
+        }
+    }
+}
