48aa9e3f9402ba4e7f68d16ae759ece81243bb4a
MPD97 <mateuszpracadom@gmail.com>
2020-06-14 15:41:17 +0200

    Added BannerEFConfiguration

---
+++ APBD-Advert-Project/AdvertDatabaseCL/Configurations/BannerEFConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdvertDatabaseCL.Configurations
+{
+    public class BannerEFConfiguration : IEntityTypeConfiguration<Banner>
+    {
+        public void Configure(EntityTypeBuilder<Banner> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
