a478d3c9ccc59aad026d922c7655195f43cd9ec9
MPD97 <mateuszpracadom@gmail.com>
2020-06-14 18:42:39 +0200

    Added PBKDF2PasswordHasher

---
+++ APBD-Advert-Project/Advert.Presistance/Services/PBKDF2PasswordHasher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Advert.Presistance.Services
+{
+    public class PBKDF2PasswordHasher : IPasswordHasher
+    {
+        public string Create(string value, string salt)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Validate(string value, string salt, string hash)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
