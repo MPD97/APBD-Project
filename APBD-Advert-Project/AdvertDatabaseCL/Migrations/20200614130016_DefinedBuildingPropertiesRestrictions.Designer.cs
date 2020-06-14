b1b6f53ad56916af1f4fcde661610f231272b863
MPD97 <mateuszpracadom@gmail.com>
2020-06-14 15:01:17 +0200

    Added DefinedBuildingPropertiesRestrictions migration

--- APBD-Advert-Project/AdvertDatabaseCL/Migrations/20200614130016_DefinedBuildingPropertiesRestrictions.Designer.cs
+++ APBD-Advert-Project/AdvertDatabaseCL/Migrations/20200614130016_DefinedBuildingPropertiesRestrictions.Designer.cs
@@ -3,14 +3,16 @@ using AdvertDatabaseCL.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AdvertDatabaseCL.Migrations
 {
     [DbContext(typeof(AdvertContext))]
-    partial class AdvertContextModelSnapshot : ModelSnapshot
+    [Migration("20200614130016_DefinedBuildingPropertiesRestrictions")]
+    partial class DefinedBuildingPropertiesRestrictions
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ APBD-Advert-Project/AdvertDatabaseCL/Migrations/20200614130016_DefinedBuildingPropertiesRestrictions.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AdvertDatabaseCL.Migrations
+{
+    public partial class DefinedBuildingPropertiesRestrictions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Street",
+                table: "Buildings",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Height",
+                table: "Buildings",
+                type: "decimal(6,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "City",
+                table: "Buildings",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Street",
+                table: "Buildings",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Height",
+                table: "Buildings",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(6,2)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "City",
+                table: "Buildings",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 100);
+        }
+    }
+}
