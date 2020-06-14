7f9c900d57c373d1a00baae312ddbd95e38c8804
MPD97 <mateuszpracadom@gmail.com>
2020-06-14 19:03:55 +0200

    Created Test Classes

---
+++ APBD-Advert-Project/Advert.PresistanceTests/Services/PBKDF2PasswordHasherTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Advert.Presistance.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Advert.Presistance.Services.Tests
+{
+    [TestClass()]
+    public class PBKDF2PasswordHasherTests
+    {
+        [TestMethod()]
+        public void CreateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ValidateTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
