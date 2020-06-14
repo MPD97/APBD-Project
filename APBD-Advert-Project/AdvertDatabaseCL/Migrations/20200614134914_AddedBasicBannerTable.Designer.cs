90266383b8c0d878b357bde6c85726cd9da30973
MPD97 <mateuszpracadom@gmail.com>
2020-06-14 15:50:07 +0200

    Added AddedBasicBannerTable migration

--- APBD-Advert-Project/AdvertDatabaseCL/Migrations/20200614134914_AddedBasicBannerTable.Designer.cs
+++ APBD-Advert-Project/AdvertDatabaseCL/Migrations/20200614134914_AddedBasicBannerTable.Designer.cs
@@ -4,14 +4,16 @@ using AdvertDatabaseCL.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AdvertDatabaseCL.Migrations
 {
     [DbContext(typeof(AdvertContext))]
-    partial class AdvertContextModelSnapshot : ModelSnapshot
+    [Migration("20200614134914_AddedBasicBannerTable")]
+    partial class AddedBasicBannerTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ APBD-Advert-Project/AdvertDatabaseCL/Migrations/20200614134914_AddedBasicBannerTable.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AdvertDatabaseCL.Migrations
+{
+    public partial class AddedBasicBannerTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Banners",
+                columns: table => new
+                {
+                    IdAdvertisment = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<int>(nullable: false),
+                    Price = table.Column<decimal>(nullable: false),
+                    IdCampaign = table.Column<int>(nullable: false),
+                    Area = table.Column<decimal>(nullable: false),
+                    CampaignIdCampaign = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Banners", x => x.IdAdvertisment);
+                    table.ForeignKey(
+                        name: "FK_Banners_Campaigns_CampaignIdCampaign",
+                        column: x => x.CampaignIdCampaign,
+                        principalTable: "Campaigns",
+                        principalColumn: "IdCampaign",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Banners_CampaignIdCampaign",
+                table: "Banners",
+                column: "CampaignIdCampaign");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Banners");
+        }
+    }
+}
