59cb053abec8d00425c4a992a46a3f7f38a37b6f
Mateusz <mateusz@o2.pl>
2020-06-15 10:31:31 +0200

    Creatd ClientRegisterService with constructor

---
+++ APBD-Advert-Project/Advert.Presistance/Services/ClientRegisterService.cs
@@ -0,0 +1,23 @@
+﻿using Advert.Database.DTOs.Requests;
+using Advert.Database.DTOs.Responses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Advert.Presistance.Services
+{
+    public class ClientRegisterService : IRegisterService
+    {
+        private readonly IPasswordHasher _passwordHasher;
+
+        public ClientRegisterService(IPasswordHasher passwordHasher)
+        {
+            _passwordHasher = passwordHasher;
+        }
+
+        public ClientRegisterResponseModel Create(ClientRegisterRequestModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
