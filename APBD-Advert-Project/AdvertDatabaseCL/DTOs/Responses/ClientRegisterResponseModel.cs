0bfa7b4471e2f341765ffaf5df3c1eb59f18d962
Mateusz <mateusz@o2.pl>
2020-06-15 10:25:43 +0200

    Created ClientRegisterResponseModel

---
+++ APBD-Advert-Project/AdvertDatabaseCL/DTOs/Responses/ClientRegisterResponseModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Advert.Database.DTOs.Responses
+{
+    public class ClientRegisterResponseModel
+    {
+        public int IdClient { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Login { get; set; }
+    }
+}
