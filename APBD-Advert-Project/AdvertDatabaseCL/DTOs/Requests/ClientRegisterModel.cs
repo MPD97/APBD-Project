8913020255f7cb1d15b8b65d6654a31f5ba212bf
Mateusz <mateusz@o2.pl>
2020-06-15 10:15:32 +0200

    Created basic ClientRegisterModel

---
+++ APBD-Advert-Project/AdvertDatabaseCL/DTOs/Requests/ClientRegisterModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Advert.Database.DTOs.Requests
+{
+    public class ClientRegisterModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Login { get; set; }
+    }
+}
