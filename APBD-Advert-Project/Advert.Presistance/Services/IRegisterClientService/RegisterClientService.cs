6a664448b46ef5d89bc9b50c3fd0409c5f9d9408
Mateusz <mateusz@o2.pl>
2020-06-15 13:01:46 +0200

    Renamed ClientRegisterService to RegisterClientService

--- APBD-Advert-Project/Advert.Presistance/Services/IRegisterClientService/RegisterClientService.cs
+++ APBD-Advert-Project/Advert.Presistance/Services/IRegisterClientService/RegisterClientService.cs
@@ -10,11 +10,11 @@ using System.Threading.Tasks;
 
 namespace Advert.Presistance.Services
 {
-    public class ClientRegisterService : IRegisterClientService
+    public class RegisterClientService : IRegisterClientService
     {
         private readonly IPasswordHasherService _passwordHasher;
         private readonly AdvertContext _context;
-        public ClientRegisterService(IPasswordHasherService passwordHasher, AdvertContext context)
+        public RegisterClientService(IPasswordHasherService passwordHasher, AdvertContext context)
         {
             _passwordHasher = passwordHasher;
             _context = context;
