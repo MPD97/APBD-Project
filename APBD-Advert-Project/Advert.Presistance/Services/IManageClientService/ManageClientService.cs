79a079132809028cfebae5a3c9527cc5a4b1ecbe
Mateusz <mateusz@o2.pl>
2020-06-15 13:03:33 +0200

    Implemented interface methods

--- APBD-Advert-Project/Advert.Presistance/Services/IManageClientService/ManageClientService.cs
+++ APBD-Advert-Project/Advert.Presistance/Services/IManageClientService/ManageClientService.cs
@@ -1,10 +1,27 @@
-﻿using System;
+﻿using Advert.Presistance.Services.IManageService;
+using AdvertDatabaseCL.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
-namespace Advert.Presistance.Services.IManageClientService
+namespace Advert.Presistance.Services
 {
-    class ManageClientService
+    public class ManageClientService : IManageClientService
     {
+        public Task<Client> Create(Client client, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Client> Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<Client>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
