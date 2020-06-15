116a5a3f3ec84523f38a3e4adebce095c32f9681
Mateusz <mateusz@o2.pl>
2020-06-15 10:33:00 +0200

    Injected AdvertContext

--- APBD-Advert-Project/Advert.Presistance/Services/ClientRegisterService.cs
+++ APBD-Advert-Project/Advert.Presistance/Services/ClientRegisterService.cs
@@ -1,5 +1,6 @@
 ﻿using Advert.Database.DTOs.Requests;
 using Advert.Database.DTOs.Responses;
+using AdvertDatabaseCL.Contexts;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,10 +10,11 @@ namespace Advert.Presistance.Services
     public class ClientRegisterService : IRegisterService
     {
         private readonly IPasswordHasher _passwordHasher;
-
-        public ClientRegisterService(IPasswordHasher passwordHasher)
+        private readonly AdvertContext _context;
+        public ClientRegisterService(IPasswordHasher passwordHasher, AdvertContext context)
         {
             _passwordHasher = passwordHasher;
+            _context = context;
         }
 
         public ClientRegisterResponseModel Create(ClientRegisterRequestModel model)
