c9b7a15f93a3dac47b773360df553ebee9e84caa
Mateusz <mateusz@o2.pl>
2020-06-16 09:59:24 +0200

    Added GetClientHandler

---
+++ APBD-Advert-Project/Advert.Presistance/Mediator/Handlers/GetClientHandler.cs
@@ -0,0 +1,18 @@
+﻿using Advert.Database.DTOs.Responses;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Advert.Presistance.Mediator.Handlers
+{
+    public class GetClientHandler : IRequest<ClientResponseModel>
+    {
+        public GetClientHandler(int clientId)
+        {
+            ClientId = clientId;
+        }
+
+        public int ClientId { get; set; }
+    }
+}
