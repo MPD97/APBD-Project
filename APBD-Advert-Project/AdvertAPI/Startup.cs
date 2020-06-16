57200f64c2d18fb3c4143331f435d8a85d678f36
Mateusz <mateusz@o2.pl>
2020-06-16 11:40:32 +0200

    Added JwBearer service

--- APBD-Advert-Project/AdvertAPI/Startup.cs
+++ APBD-Advert-Project/AdvertAPI/Startup.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Advert.Database.MapProfiles;
 using Advert.Presistance.Services;
@@ -8,6 +9,7 @@ using Advert.Presistance.Services.IManageService;
 using AdvertDatabaseCL.Contexts;
 using AutoMapper;
 using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -16,6 +18,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 namespace AdvertAPI
@@ -32,6 +35,19 @@ namespace AdvertAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                   .AddJwtBearer(options =>
+                   {
+                       options.TokenValidationParameters = new TokenValidationParameters
+                       {
+                           ValidateIssuer = true,
+                           ValidateAudience = true,
+                           ValidateLifetime = true,
+                           ValidIssuer = "Advert",
+                           ValidAudience = "Clients",
+                           IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JwtSettings:secret"]))
+                       };
+                   });
             services.AddDbContext<AdvertContext>(config =>
                 config.UseSqlServer(Configuration.GetConnectionString("default")));
             services.AddSingleton<IPasswordHasherService, PBKDF2PasswordHasherService>();
@@ -66,6 +82,7 @@ namespace AdvertAPI
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
