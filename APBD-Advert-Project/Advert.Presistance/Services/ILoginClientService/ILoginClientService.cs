8eb7e9fc4ff88e8237f7fdc2a5eb9dc271270ac5
Mateusz <mateusz@o2.pl>
2020-06-16 12:02:27 +0200

    IloginClientService and IJwtBearerService Added

--- APBD-Advert-Project/Advert.Presistance/Services/ILoginClientService/ILoginClientService.cs
+++ APBD-Advert-Project/Advert.Presistance/Services/ILoginClientService/ILoginClientService.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using Advert.Database.DTOs.Requests;
+using AdvertDatabaseCL.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Advert.Presistance.Services.ILoginClientService
 {
     public interface ILoginClientService
     {
+        public Task<Client> Login(ClientLoginRequestModel model);
     }
 }
