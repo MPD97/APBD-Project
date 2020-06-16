2ab48bb71cc8ba2872c51b221d221b77ca70a7ec
Mateusz <mateusz@o2.pl>
2020-06-16 13:13:28 +0200

    Created LoginClientService

---
+++ APBD-Advert-Project/Advert.Presistance/Services/ILoginClientService/LoginClientService.cs
@@ -0,0 +1,30 @@
+﻿using Advert.Database.DTOs.Requests;
+using Advert.Presistance.Services.IJwtBarerService;
+using AdvertDatabaseCL.Contexts;
+using AdvertDatabaseCL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advert.Presistance.Services.ILoginClientService
+{
+    public class LoginClientService : ILoginClientService
+    {
+        private readonly IPasswordHasherService _passwordHasher;
+        protected internal readonly AdvertContext _context;
+        private readonly IJwtBearerService _jwtBearer;
+
+        public LoginClientService(IPasswordHasherService passwordHasher, AdvertContext context, IJwtBearerService jwtBearer)
+        {
+            _passwordHasher = passwordHasher;
+            _context = context;
+            _jwtBearer = jwtBearer;
+        }
+
+        public Task<Client> Login(ClientLoginRequestModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
