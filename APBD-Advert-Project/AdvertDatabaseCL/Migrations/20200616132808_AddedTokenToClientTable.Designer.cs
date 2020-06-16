35394b6f383733762355b60b827633424dd6ebe1
Mateusz <mateusz@o2.pl>
2020-06-16 15:28:26 +0200

    Added AddedTokenToClientTable migration

--- APBD-Advert-Project/AdvertDatabaseCL/Migrations/20200616132808_AddedTokenToClientTable.Designer.cs
+++ APBD-Advert-Project/AdvertDatabaseCL/Migrations/20200616132808_AddedTokenToClientTable.Designer.cs
@@ -4,14 +4,16 @@ using AdvertDatabaseCL.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AdvertDatabaseCL.Migrations
 {
     [DbContext(typeof(AdvertContext))]
-    partial class AdvertContextModelSnapshot : ModelSnapshot
+    [Migration("20200616132808_AddedTokenToClientTable")]
+    partial class AddedTokenToClientTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
