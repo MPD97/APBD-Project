b00a5db957a94f66904a1608c2b39367ebbb5767
Mateusz <mateusz@o2.pl>
2020-06-17 10:16:03 +0200

    Created CampaignQueryService

---
+++ APBD-Advert-Project/Advert.Presistance/Services/ICampaignQuery/CampaignQueryService.cs
@@ -0,0 +1,22 @@
+﻿using Advert.Presistance.Services.ICampaignService;
+using AdvertDatabaseCL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advert.Presistance.Services.ICampaignQuery
+{
+    public class CampaignQueryService : ICampaignQueryService
+    {
+        public Task<Campaign> Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Campaign>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
