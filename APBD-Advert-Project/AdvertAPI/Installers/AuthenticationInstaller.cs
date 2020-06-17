220e1d2e326785e4223ce50fcbeba1f1447ea75a
Mateusz <mateusz@o2.pl>
2020-06-17 14:59:02 +0200

    Added services installer

---
+++ APBD-Advert-Project/AdvertAPI/Installers/AuthenticationInstaller.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Advert.API.Installers
+{
+    public class AuthenticationInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration configuration)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
