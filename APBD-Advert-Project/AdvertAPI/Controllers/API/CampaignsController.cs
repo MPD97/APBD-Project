bbb97b856815f3ddb440a66ca8d31f8f01a09662
Mateusz <mateusz@o2.pl>
2020-06-17 10:27:28 +0200

    Created actions and constructor

--- APBD-Advert-Project/AdvertAPI/Controllers/API/CampaignsController.cs
+++ APBD-Advert-Project/AdvertAPI/Controllers/API/CampaignsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -13,6 +14,23 @@ namespace Advert.API.Controllers.API
     [Authorize]
     public class CampaignsController : ControllerBase
     {
-        
+        private readonly IMediator _mediator;
+
+        public CampaignsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id) 
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("")]
+        public async Task<IActionResult> GetAll()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
