4448de5a714d8f001f421c824324fdf1f55eff06
Mateusz <mateusz@o2.pl>
2020-06-17 15:43:01 +0200

    Implementing Api versining

---
+++ APBD-Advert-Project/AdvertAPI/Contracts/V1/ApiRoutes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Advert.API.Contracts.V1
+{
+    public static class ApiRoutes
+    {
+        public const string Root = "api";
+
+        public const string Version = "v1";
+
+        public static readonly string Base = $"{Root}/{Version}";
+    }
+}
