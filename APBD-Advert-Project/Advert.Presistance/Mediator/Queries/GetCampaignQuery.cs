de8a3902bfe31edeba1b02d2a230dd18ea90b953
Mateusz <mateusz@o2.pl>
2020-06-17 10:29:14 +0200

    Added GetCampaignQuery

---
+++ APBD-Advert-Project/Advert.Presistance/Mediator/Queries/GetCampaignQuery.cs
@@ -0,0 +1,12 @@
+﻿using AdvertDatabaseCL.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Advert.Presistance.Mediator.Queries
+{
+    public class GetCampaignQuery :IRequest<Campaign>
+    {
+    }
+}
