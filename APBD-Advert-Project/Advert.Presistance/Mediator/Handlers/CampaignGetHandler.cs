a612564a144d650486a356dfda7fed77caa4d8c1
Mateusz <mateusz@o2.pl>
2020-06-17 10:30:36 +0200

    Added CampaignGetHandler

---
+++ APBD-Advert-Project/Advert.Presistance/Mediator/Handlers/CampaignGetHandler.cs
@@ -0,0 +1,19 @@
+﻿using Advert.Presistance.Mediator.Queries;
+using AdvertDatabaseCL.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Advert.Presistance.Mediator.Handlers
+{
+    public class CampaignGetHandler : IRequestHandler<GetCampaignQuery, Campaign>
+    {
+        public Task<Campaign> Handle(GetCampaignQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
