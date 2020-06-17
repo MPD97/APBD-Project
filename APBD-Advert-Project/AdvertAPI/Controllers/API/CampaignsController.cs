6bb32e4ea83f0238be3a2cdad59bedf5445201fe
Mateusz <mateusz@o2.pl>
2020-06-17 09:57:18 +0200

    Controller created

---
+++ APBD-Advert-Project/AdvertAPI/Controllers/API/CampaignsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Advert.API.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class CampaignsController : ControllerBase
+    {
+        
+    }
+}
