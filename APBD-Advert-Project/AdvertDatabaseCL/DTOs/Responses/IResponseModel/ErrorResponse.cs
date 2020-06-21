8364152539873c38e15dc493d0d4eb7926cc229e
MPD97 <mateuszpracadom@gmail.com>
2020-06-21 16:09:30 +0200

    Created IRespone interface with success and error response

---
+++ APBD-Advert-Project/AdvertDatabaseCL/DTOs/Responses/IResponseModel/ErrorResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Advert.Database.DTOs.Responses
+{
+    public class ErrorResponse : IResponseModel
+    {
+        public ResponseStatus Status { get; set; } = ResponseStatus.Error;
+        public string Message { get; set; } = null;
+        public object Result { get; set; }
+    }
+}
---
+++ APBD-Advert-Project/AdvertDatabaseCL/DTOs/Responses/IResponseModel/SuccessResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Advert.Database.DTOs.Responses
+{
+    public class SuccessResponse : IResponseModel
+    {
+        public ResponseStatus Status { get; set; } = ResponseStatus.Ok;
+        public string Message { get; set; } = null;
+        public object Result { get; set; }
+    }
+}
