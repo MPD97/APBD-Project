ddc2ece6ba724f1f27ecd9eb1ce72ab21d75c2ed
Mateusz <mateusz@o2.pl>
2020-06-24 11:24:43 +0200

    Added ClientRepository.cs

---
+++ APBD-Advert-Project/Advert.Presistance/Services/IRepository/ClientRepository.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Advert.Database.Contexts;
+using Advert.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Advert.Presistance.Services.IRepository
+{
+    public class ClientRepository : IClientRepository
+    {
+        private readonly AdvertContext _context;
+
+        public ClientRepository(AdvertContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Client> GetAll()
+        {
+            return _context.Clients.AsEnumerable();
+        }
+
+        public async Task<Client> FindByIdAsync(int id)
+        {
+            return await _context.Clients.FindAsync(id);
+        }
+
+        public async Task Insert(Client client)
+        {
+            await _context.Clients.AddAsync(client);
+        }
+
+        public async Task Insert(IEnumerable<Client> clients)
+        {
+            await _context.Clients.AddRangeAsync(clients);
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            var client = await _context.Clients.FindAsync(id);
+            if (client == null) return false;
+
+            _context.Clients.Remove(client);
+            return true;
+        }
+
+        public void Update(Client client)
+        {
+            _context.Entry(client).State = EntityState.Modified;
+        }
+
+        public async Task<int> SaveAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ APBD-Advert-Project/Advert.Presistance/Services/IRepository/IClientRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Advert.Database.Entities;
+
+namespace Advert.Presistance.Services.IRepository
+{
+    public interface IClientRepository
+    {
+        IEnumerable<Client> GetAll();
+        Task<Client> FindByIdAsync(int id);
+        Task Insert(Client client);
+        Task Insert(IEnumerable<Client> clients);
+        Task<bool> Delete(int id);
+        void Update(Client client);
+        Task<int> SaveAsync();
+    }
+}
