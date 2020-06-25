fe64b71800587d346b59f7cfa8bff4d77e2db6ba
Mateusz <mateusz@o2.pl>
2020-06-25 11:41:15 +0200

    BannerCreateService.cs Now uses IBannerRepository.cs

--- APBD-Advert-Project/Advert.Presistance/Services/IBannerCreate/BannerCreateService.cs
+++ APBD-Advert-Project/Advert.Presistance/Services/IBannerCreate/BannerCreateService.cs
@@ -1,28 +1,29 @@
 ﻿using System.Threading.Tasks;
-using Advert.Database.Contexts;
 using Advert.Database.DTOs.Requests;
 using Advert.Database.Entities;
+using Advert.Presistance.Services.IRepository;
 using AutoMapper;
 
 namespace Advert.Presistance.Services.IBannerCreate
 {
     public class BannerCreateService : IBannerCreateService
     {
-        private readonly AdvertContext _context;
         private readonly IMapper _mapper;
+        private readonly IBannerRepository _repository;
 
-        public BannerCreateService(IMapper mapper, AdvertContext context)
+        public BannerCreateService(IBannerRepository repository, IMapper mapper)
         {
+            _repository = repository;
             _mapper = mapper;
-            _context = context;
         }
+
 
         public async Task<Banner> Create(BannerCreateRequestModel model)
         {
             var banner = _mapper.Map<Banner>(model);
-            _context.Add((object) banner);
+            await _repository.Insert(banner);
 
-            if (await _context.SaveChangesAsync() == 0) return null;
+            if (await _repository.SaveAsync() == 0) return null;
 
             return banner;
         }
