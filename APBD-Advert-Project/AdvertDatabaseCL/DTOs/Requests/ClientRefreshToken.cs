458d3f7a2b19f8160c321c6d8b30f837552a7125
Mateusz <mateusz@o2.pl>
2020-06-26 11:36:50 +0200

    remove unused annotations

--- APBD-Advert-Project/AdvertDatabaseCL/DTOs/Requests/ClientRefreshToken.cs
+++ APBD-Advert-Project/AdvertDatabaseCL/DTOs/Requests/ClientRefreshToken.cs
@@ -1,11 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Advert.Database.DTOs.Requests
+﻿namespace Advert.Database.DTOs.Requests
 {
     public class ClientRefreshToken
     {
-        [Required] public string Token { get; set; }
+        public string Token { get; set; }
 
-        [Required] public string RefreshToken { get; set; }
+        public string RefreshToken { get; set; }
     }
 }
