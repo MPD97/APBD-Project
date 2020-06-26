88783c3fd45388dd79c8a208835ae830faef139d
Mateusz <mateusz@o2.pl>
2020-06-26 10:31:58 +0200

    ClientRegisterRequestValidator

--- APBD-Advert-Project/AdvertDatabaseCL/DTOs/Requests/ClientRegisterRequest.cs
+++ APBD-Advert-Project/AdvertDatabaseCL/DTOs/Requests/ClientRegisterRequest.cs
@@ -4,40 +4,18 @@ namespace Advert.Database.DTOs.Requests
 {
     public class ClientRegisterRequest
     {
-        [Required]
-        [MinLength(2)]
-        [MaxLength(15)]
         public string FirstName { get; set; }
 
-        [Required]
-        [MinLength(2)]
-        [MaxLength(15)]
         public string LastName { get; set; }
 
-        [Required]
-        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
-        [Required]
-        [DataType(DataType.PhoneNumber)]
         public string Phone { get; set; }
 
-        [Required]
-        [MinLength(6)]
-        [MaxLength(15)]
         public string Login { get; set; }
 
-        [Required]
-        [MinLength(8)]
-        [MaxLength(20)]
-        [DataType(DataType.Password)]
-        public string Password { get; set; }
+        [DataType(DataType.Password)] public string Password { get; set; }
 
-        [Required]
-        [MinLength(8)]
-        [MaxLength(20)]
-        [DataType(DataType.Password)]
-        [Compare("Password")]
-        public string RepeatPassword { get; set; }
+        [DataType(DataType.Password)] public string RepeatPassword { get; set; }
     }
 }
